c5b21b712b101969510ea871d7db75d4ee84d0df
Brent Nelson <bnelson.fc7@gmail.com>
2023-04-15 09:02:01 -0500

    Read in board from file and choose random move

--- src/main.java
+++ src/main.java
@@ -1,28 +1,34 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 import objects.Board;
+import objects.Location;
+import objects.Move;
+import objects.pieces.AbstractPiece;
 
 public class main {
 
 	public static void main(String[] args) 
 	{
-		String stringBoard = "+-+-+-+-+-+-+-+-+\n"
-				+ "|r|n|b|q|k|b|n|r|\n"
-				+ "+-+-+-+-+-+-+-+-+\n"
-				+ "|p|p|p|p|p|p|p|p|\n"
-				+ "+-+-+-+-+-+-+-+-+\n"
-				+ "| | | | | | | | |\n"
-				+ "+-+-+-+-+-+-+-+-+\n"
-				+ "| | | | | | | | |\n"
-				+ "+-+-+-+-+-+-+-+-+\n"
-				+ "| | | | | | | | |\n"
-				+ "+-+-+-+-+-+-+-+-+\n"
-				+ "| | | | | | | | |\n"
-				+ "+-+-+-+-+-+-+-+-+\n"
-				+ "|P|P|P|P|P|P|P|P|\n"
-				+ "+-+-+-+-+-+-+-+-+\n"
-				+ "|R|N|B|Q|K|B|N|R|\n"
-				+ "+-+-+-+-+-+-+-+-+\n";
+		File boardFile = new File("files\\board.txt");
+	    Scanner scanner;
+	    String stringBoard = "";
+		try {
+			scanner = new Scanner(boardFile);
+			while (scanner.hasNext()) {
+		    	stringBoard += scanner.nextLine() + "\n";
+		    }
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+
 		System.out.println(stringBoard);
 		Board board = new Board(stringBoard);
+		Move move = board.getRandomMove();
+		Location start = move.getStartLocation();
+		AbstractPiece piece = board.getPiece(start.getX(), start.getY());
+		System.out.println("Move "+piece+" from " + move);
 	}
 
 }
